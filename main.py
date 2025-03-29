528fab6b01896824c0b10e10420c1788cc21da19
martinsvaikuls <144250487+martinsvaikuls@users.noreply.github.com>
2025-03-30 00:07:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import random
 
 
 class gameNode:
-    def __init__(self, number, p1, p2, nextTurn, left, right, staticValue, dynamicValue):      # p1 = player, p2 = computer
-        self.number = number                                        # p1 | num | p2
-        self.p1 = p1                                                #  0 | 999 | 0
-        self.p2 = p2                                                
-        self.nextTurn = nextTurn    
-        self.left = left                                          
-        self.right = right
-        self.staticValue = staticValue
+    def __init__(self, number, player1Pts, player2Pts, currentPlayer, left, right, heuristicValue, dynamicValue):      # player1Pts = player, player2Pts = computer
+        self.number = number            # player1Pts | num | player2Pts
+        self.player1Pts = player1Pts    #  0 | 999 | 0
+        self.player2Pts = player2Pts                                                
+        self.currentPlayer = currentPlayer    
+        self.left = left   # Virsotne                                       
+        self.right = right 
+        self.heuristicValue = heuristicValue
         self.dynamicValue = dynamicValue
         
 
@@ -17,36 +17,37 @@ class gameNode:
 
 class gameTree:
     def __init__(self, number):
-        self.root = self.build(gameNode(number, 0, 0, "player", None, None, 0, -3621), 1)
+        self.root = self.build(gameNode(number, 0, 0, "player", None, None, 0, -6000), 1)
         self.print_tree(self.root)
     
 
     def build(self, node, num):
         gameList = ["player","computer"]
-                # ja bus svarigi
         nt = gameList[num]
+
         if (node.number >= 10):
             if (num == 0):
                 num = 1
                 if(node.number % 2 == 0):
-                    staticValue = node.p1 - node.p2 + 0
-                    node.right = gameNode(node.number//2, node.p1+2, node.p2, nt, None, None, staticValue, -3621)
+                    heuristicValue = node.player1Pts - node.player2Pts + 0
+                    node.right = gameNode(node.number//2, node.player1Pts+2, node.player2Pts, nt, None, None, heuristicValue, -6000)
                     self.build(node.right, num)
 
                 if(node.number % 3 == 0):
-                    staticValue = node.p1 - node.p2 + 2
-                    node.left = gameNode(node.number//3, node.p1, node.p2+3, nt, None, None, staticValue, -3621)
+                    heuristicValue = node.player1Pts - node.player2Pts + 2
+                    node.left = gameNode(node.number//3, node.player1Pts, node.player2Pts+3, nt, None, None, heuristicValue, -6000)
                     self.build(node.left, num)
+
             else:
                 num = 0
                 if(node.number % 2 == 0):
-                    staticValue = node.p1 - node.p2 + 0
-                    node.right = gameNode(node.number//2, node.p1, node.p2+2, nt, None, None, staticValue, -3621)
+                    heuristicValue = node.player1Pts - node.player2Pts + 0
+                    node.right = gameNode(node.number//2, node.player1Pts, node.player2Pts+2, nt, None, None, heuristicValue, -6000)
                     self.build(node.right, num)
 
                 if(node.number % 3 == 0):
-                    staticValue = node.p1 - node.p2 + 2
-                    node.left = gameNode(node.number//3, node.p1+3, node.p2, nt, None, None, staticValue, -3621)
+                    heuristicValue = node.player1Pts - node.player2Pts + 2
+                    node.left = gameNode(node.number//3, node.player1Pts+3, node.player2Pts, nt, None, None, heuristicValue, -6000)
                     self.build(node.left, num)
             
             return node
@@ -57,7 +58,7 @@ class gameTree:
         #"""
         if node is not None:
             self.print_tree(node.right, level + 1)
-            print(' ' * 4 * level + '->', node.number, node.nextTurn, node.p1, node.p2, node.staticValue)
+            print(' ' * 4 * level + '->', node.number, node.currentPlayer, node.player1Pts, node.player2Pts, node.heuristicValue)
             self.print_tree(node.left, level + 1)
         #"""
      # --- CHAT GPT
@@ -66,24 +67,24 @@ class gameTree:
 # Nemts no interneta video
 def minimax(ntraversedNodes, node, depth, isMaximisingPlayer):
     if (depth == 0 or ((node.left == None and node.right == None))):
-        node.dynamicValue = node.staticValue
-        return ntraversedNodes, node.staticValue
+        node.dynamicValue = node.heuristicValue
+        return ntraversedNodes, node.heuristicValue
     
-    #print(ntraversedNodes)
+    
     if isMaximisingPlayer:
         maxEval = -10000
         if (node.left != None):
             ntraversedNodes, eval = minimax(ntraversedNodes, node.left, depth - 1, False)
             maxEval = max(maxEval, eval)
             ntraversedNodes+=1
-        #print(ntraversedNodes)
+        
         if (node.right != None):
             ntraversedNodes,eval = minimax(ntraversedNodes, node.right, depth - 1, False)
             maxEval = max(maxEval, eval)
             ntraversedNodes+=1
 
+
         node.dynamicValue = maxEval
-        #print(ntraversedNodes)
         return ntraversedNodes, maxEval
     
     else:
@@ -98,15 +99,15 @@ def minimax(ntraversedNodes, node, depth, isMaximisingPlayer):
             minEval = min(minEval, eval)
             ntraversedNodes+=1
 
+
         node.dynamicValue = minEval
-        #print(ntraversedNodes)
         return ntraversedNodes, minEval
     
 
 def alphaBeta(ntraversedNodes, node, depth, isMaximisingPlayer, alpha, beta):
     if (depth == 0 or ((node.left == None and node.right == None))):
-            node.dynamicValue = node.staticValue
-            return ntraversedNodes, node.staticValue
+            node.dynamicValue = node.heuristicValue
+            return ntraversedNodes, node.heuristicValue
         
     if isMaximisingPlayer:
         maxEval = -10000
@@ -153,7 +154,6 @@ def alphaBetaMin(ntraversedNodes, node, depth, isMaximisingPlayer, alpha, beta, 
 
 # komentari šim un gui ir ģenerēti :)
 
-
 class GameEngine:
     def __init__(self, start_number, algorithm=1, player_vs_ai=True):
         # Inicializē spēles dzinēju ar sākuma skaitli, algoritma izvēli un spēles režīmu (cilvēks pret datoru)
@@ -163,22 +163,24 @@ class GameEngine:
         self.player_vs_ai = player_vs_ai    # Saglabā informāciju par to, vai spēle ir cilvēks pret datoru
         self.algorithm = algorithm          # Saglabā izvēlēto algoritmu (1 vai 2)    
         self.traversedNodes = 0
+        print("init",self.player_vs_ai)
 
     def get_state(self):
         # Atgriež pašreizējo spēles stāvokli kā vārdnīcu, ko var izmantot GUI interfeisā
+        print("getting state", self.player_vs_ai)
         if self.player_vs_ai:
             return {
-                "skaitlis": self.current.number,           # Pašreizējais skaitlis
-                "spēlētājs": self.current.p1,         # Spēlētāja punkti
-                "dators": self.current.p2,       # Datora punkti
-                "turn": self.current.nextTurn            # Kurš veic nākamo gājienu ("player" vai "computer")
+                "skaitlis": self.current.number,         # Pašreizējais skaitlis
+                "cilvēks": self.current.player1Pts,      # Spēlētāja punkti
+                "dators": self.current.player2Pts,       # Datora punkti
+                #"turn": self.current.currentPlayer       # Kurš veic nākamo gājienu 
             }
         else:
             return {
-                "skaitlis": self.current.number,           # Pašreizējais skaitlis
-                "dators1": self.current.p1,         # Spēlētāja punkti
-                "dators2": self.current.p2,       # Datora punkti
-                "turn": self.current.nextTurn            # Kurš veic nākamo gājienu ("player" vai "computer")
+                "skaitlis": self.current.number,         # Pašreizējais skaitlis
+                "dators1": self.current.player1Pts,      # Spēlētāja punkti
+                "dators2": self.current.player2Pts,      # Datora punkti
+                #"turn": self.current.currentPlayer       # Kurš veic nākamo gājienu 
             }
 
     def is_game_over(self):
@@ -190,7 +192,7 @@ class GameEngine:
         
         # Veic spēlētāja gājienu, ja tas ir atļauts:
         # Pārbauda, vai nākamais gājiens ir spēlētājam un vai pašreizējais skaitlis dalās ar norādīto divisor (2 vai 3).
-        if self.current.nextTurn == "player" and self.current.number % divisor == 0 :
+        if self.current.currentPlayer == "player" and self.current.number % divisor == 0 :
             self.current.number //= divisor  # Dalās ar divisor, atjaunojot skaitli
             # Pāriet uz atbilstošo apakšmezglu (koka mezglu) atkarībā no izvēlētā dalītāja
             if divisor == 3 and self.current.left is not None:
@@ -202,82 +204,62 @@ class GameEngine:
         return False  # Ja nosacījumi netiek izpildīti, gājiens nav veiksmīgs
 
     def make_ai_move(self):
-        # Veic datora (AI) gājienu, ja nākamais gājiens ir datoram
-        #if self.current.nextTurn == "computer":
-            # Izvēlas algoritmu atkarībā no izvēles (1 = minimax, 2 = alphaBeta)
+        # Veic datora (AI) gājienu, ja nākamais gājiens ir "computer" tad minimizē, ja "player" tad maximizē
         ntraversedNodes = 0
+
         isMaximising = False
-        if self.current.nextTurn == "computer":
+        if self.current.currentPlayer == "computer":
             isMaximising = False
         else:
             isMaximising = True
 
-        goTroughAlgorythm = True
 
-        
+        goTroughAlgorythm = True        
         try:
-            if (self.current.left.dynamicValue == self.current.dynamicValue and not (self.current.dynamicValue == -3621)):
-                if not (self.current.left.dynamicValue == -3621):
+            if (self.current.left.dynamicValue == self.current.dynamicValue and not (self.current.dynamicValue == -6000)):
+                if not (self.current.left.dynamicValue == -6000):
                     goTroughAlgorythm = False
                 else:
                     goTroughAlgorythm = True
-                
         except:
             pass
+
         try:    
-            if self.current.right.dynamicValue == self.current.dynamicValue and not (self.current.dynamicValue == -3621): 
-                if not (self.current.right.dynamicValue == -3621):
+            if self.current.right.dynamicValue == self.current.dynamicValue and not (self.current.dynamicValue == -6000): 
+                if not (self.current.right.dynamicValue == -6000):
                     goTroughAlgorythm = False
                 else:
                     goTroughAlgorythm = True
         except:
             pass
 
+
+        SEARCH_DEPTH = 10
         if goTroughAlgorythm:
-            #print("algo")
+
             if self.algorithm == 1:
-                ntraversedNodes, eval = minimax(ntraversedNodes, self.current, 40, isMaximising)  # Izsauc minimax algoritmu ar dziļumu 4
-                #print('minimax')
+                ntraversedNodes, eval = minimax(ntraversedNodes, self.current, SEARCH_DEPTH, isMaximising) 
+
             else:
-                ntraversedNodes, eval = alphaBeta(ntraversedNodes, self.current, 40, isMaximising, -10000, 10000)  # Izsauc alphaBeta algoritmu ar dziļumu 4 un sākotnējiem alpha, beta vērtībām
-                #print('alfabeta')
-                
+                ntraversedNodes, eval = alphaBeta(ntraversedNodes, self.current, SEARCH_DEPTH, isMaximising, -10000, 10000)
+
         print(ntraversedNodes)
-         # Pārbauda, kurš apakšmezgls atbilst aprēķinātajam vērtējumam:
-        # Ja kreisais (dalīšana ar 2) apakšmezgls ir derīgs un tā dinamiskā vērtība sakrīt ar pašreizējo, tad veic gājienu
-        """
-        print(self.current.nextTurn)       
-        print(self.current.dynamicValue)
-
         
-        try:
-            print("left ",self.current.left.dynamicValue, end="")
-        except:
-            pass
 
-        try:
-            print(" right",self.current.right.dynamicValue)
-        except:
-            pass
-        """
-        
-        # Pretējā gadījumā, ja labo (dalīšana ar 3) apakšmezgls atbilst, tad veic gājienu
+        # Pārbauda, kurš apakšmezgls atbilst aprēķinātajam vērtējumam:
+
+        # Ja Kreisais (dalīšana ar 3) apakšmezgls ir derīgs un tā dinamiskā vērtība sakrīt ar pašreizējo, tad veic gājienu
         if self.current.left is not None and self.current.left.dynamicValue == self.current.dynamicValue:
-            #print("left")
             self.current.number //= 3
             self.current = self.current.left
+
+        # Pretējā gadījumā, ja labo (dalīšana ar 3) apakšmezgls atbilst, tad veic gājienu        
         elif self.current.right is not None and self.current.right.dynamicValue == self.current.dynamicValue:
-            #print("right")
             self.current.number //= 2
             self.current = self.current.right
         
         
         
-
-        
-       
-
-
-        
+   
 if __name__ == '__main__':
     main()
